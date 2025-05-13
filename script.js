57ac993830f94b8c7ed3f4b0da1e8917a62965aa
12hellyeah12 <113688239+12hellyeah12@users.noreply.github.com>
2025-05-13 15:43:15 +0200

    Retrieve form data, process it and print result

--- script.js
+++ script.js
@@ -11,7 +11,66 @@ function showTabs(){
     const tabNavigation = document.querySelector('#tab');
     const landingPage = document.querySelector('#landing');
 
-    tabNavigation.style.display = 'block';
+    tabNavigation.style.display = 'flex';
     landingPage.style.display = 'none';
     tabs[0].classList.add('active');
+}
+
+/* Get form data and validate it */
+function retrieveFormData(){
+        const privacyForm = document.querySelector('#privacy-mini-quizz');
+        const privacyMiniQuizz = new FormData(privacyForm);
+        const formInput = [];
+        for(const value of privacyMiniQuizz.values()){
+            formInput.push(value);
+        }
+    
+        if(formInput.length < 8){
+            alert('Please answer all questions');
+        } else{
+            return formInput;
+        }
+}
+
+function validateQuizzAnswers(){
+    const userAnswers = retrieveFormData();
+    const correctAnswers = ['b','b','b','c','c','b','a','b'];
+    let validatedAnswers = [];
+
+    validatedAnswers = correctAnswers.map((answer, index) => {
+        return userAnswers[index] === answer ? true : false;
+    });
+
+    return validatedAnswers;
+}
+
+function printQuizzResults(event){
+    event.preventDefault();
+    const validatedAnswers = validateQuizzAnswers();
+    const quizResult = document.querySelector('#quiz-result');
+    const privacyQuestions = document.querySelectorAll('#privacy-mini-quizz > fieldset');
+    let result = 0;
+    let passed = false;
+    
+    privacyQuestions.forEach((question, index) => {
+        validatedAnswers[index] === true 
+        ? question.style.borderLeftColor = 'rgb(61, 135, 61)'
+        : question.style.borderLeftColor = 'rgb(168, 55, 55)';
+    });
+
+    for(let i = 0; i < validatedAnswers.length; i++){
+        if(validatedAnswers[i] === true){
+            result++;
+        }
+    }
+
+    if(result / validatedAnswers.length < 0.8){
+        quizResult.textContent = 'Dranbleiben. Versuche es nochmal!'
+        quizResult.style.color = 'rgb(168, 55, 55)';
+        passed = false;
+    } else {
+        quizResult.textContent = 'Herzlichen Glückwunsch! Du hast den Test bestanden.'
+        quizResult.style.color = 'rgb(61, 135, 61)';
+        passed = true;
+    }
 }
